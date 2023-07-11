2245d2a88fa99c77f8028d58e2ca456936378d3f
tamimmurad <ta4818al-s@student.lu.se>
2023-07-11 12:07:20 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Viral Proteins Sequence Based Clustering
+
+## Description
+This repository contains the following:
+- Methodology for acquiring viral protein sequences, clustering them and select top 10k clusters (in terms of number of members in a cluster).
+- Fastafiles both used and produced.
+- Tabular files of cluster reps and members.
+- A Python script to select cluster reps from clusters.
+For this work the following tools were used:
+	- MMseq2: to cluster the proteins and get preliminary cluster reps.
+		https://github.com/soedinglab/MMseqs2
+	- Seqkit: to get statists about the sequences and sort them. 
+		https://bioinf.shenwei.me/seqkit/
+
+
+## Methodology
+
+### Download viral protein sequences.
+
+- Using NCBI protein database, all fasta files of proteins with tax id 10239 (organism: Viruses) and with RefSeq entry were downloaded.
+
+https://www.ncbi.nlm.nih.gov/protein/?term=(txid10239%5BOrganism%5D)+AND+%22refseq%22%5BFilter%5D
+
+NOTE:Downloaded on 17th of April including 647,000 sequences.
+
+- To include in our clustering already predicted and determined structures, viral sequences in PDB and in AlphaFold were downloaded. 
+    For PDB, sequences were downloaded from NCBI as it has an entry for every chain.
+	https://www.ncbi.nlm.nih.gov/protein
+
+	Advance searh: txid10239[Organism] AND pdb[filter]
+
+	NOTE:Downloaded on 19th of April including 65,159 sequences. 
+
+	https://www.uniprot.org/uniprotkb?query=%28taxonomy_id%3A10239%29%20AND%20%28database%3Aalphafolddb%29
+
+NOTE: File was downloaded on 20th of April 2023 including 39 sequences.
+
+- File were concatenated to one fasta file with a total of 712,198 sequences and sorted with seqkit in terms of sequence length. The file name is AFDB_refsec_pdb_sorted_viral_p.fasta
+- A final curation step was steps was done to have the pdb sequence ids to conform to the format xxxx_yy is done as follows:
+    $cat AFDB_refsec_pdb_sorted_viral_p.fasta |sed 's/pdb|//'|tr '|' '_' >>all_vira_sequences_formatted.fasta
+
+### Sequence Statstics
+
+Seqkit is used to get statistics of the sequences with the following commands:
+
+    #Get a log-scale histogram.
+	$seqkit watch -f ReadLen  -L -B 1000 AFDB_refsec_pdb_sorted_viral_p.fasta -O seq_len_his_log.png
+
+    # Get a normal scale histomgram.
+	$seqkit watch -f ReadLen  -B 1000 AFDB_refsec_pdb_sorted_viral_p.fasta -O seq_len_his.png
+
+![Alt text](seq_len_his.png "Viral Protein Sequence Lengths Histogram")
+
+![Alt text](seq_len_his_log.png "Viral Protein Sequence Lengths Histogram (log scale)")
+
+### Clustering
+
+With MMSeq2, the below command was run to get the clusters:
+
+	$mmseqs easy-cluster all_vira_sequences_formatted.fasta vp_clusters tmp --min-seq-id 0.3 -c 0.01 --cov-mode 3 --threads 36
+
+
+The choice of coverage mode is to minimize the number of clusters and yet having good separation with 30% identity. A total of 118,443 were produced.
+Note: As the command is run on a 48-cores machine, using 36 threads was possible. On the same machine it was around 14 times faster to run with 36 cores compared to 1 core.
+
+The above command produced 3 files:
+
+- vp_clusters_all_seqs.fasta: fasta files containging cluster rep sequence ID and then the ids and sequences of each member.
+- vp_clusters_rep_seq.fasta: Sequences of only the representatives.
+- vp_clusters_cluster.tsv : Two columns with one row for each sequence. The first column is the cluster rep id and the send is the sequence id.
+
+Using the last file, a file with the number of members in each cluster was generated with the below bash command:
+
+	$cat vp_clusters_cluster.tsv | awk '{print $1}'|sort|uniq -c |awk '{print $2"\t"$1}'>cluster_sizes.csv
+  
+### Selecting Top Clusters Representative with select_clus_reps.py
+
+A python script (select_clus_reps.py) was used to do the following:
+- From the cluster_stat.csv file, make a histograme and get cluster sizes stats.
+- Select largest 10k clusters (by identifying their reps).
+- Select reps with prioritizing a determined or predicted sequence to be a Representative when available in a cluster. 
+- Write the cluster reps in the file: top_viral_protein_cluster_reps.tsv. This resulted in 8956 refseq viral proteins to be folded. The remaining proteins are already determined (1014 PDB structures) or predicted in Alpha Fold DB (39 structures).
+- Finally, get the fasta sequences of the proteins to be folded by using the clustering results fasta file (linearized) with the below command:
+
+    $cat top_viral_protein_cluster_reps.tsv | awk '{print $4}'| grep '\.' |while read accn ;  do cat vp_clusters_rep_seq.fasta | grep -A1 -m 1 $accn >> top_cluster_reps_to_fold.fasta ; done 
+
+![Alt text](cluster_size_hist_log.png "Histogram of Clusters Sizes")
+
+
---
+++ select_clus_reps.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Script Name: select_clus_rep.py
+
+Description:
+The code reads in two input files, vp_clusters_cluster.tsv(clusters' reps and clusters' members in 
+ tabular format) and cluster_sizes.csv,and performs several operations on them. The first file
+ contains information about protein clusters produced by MMSeq, including a cluster 
+ representative and cluster members. The second file contains information about 
+ cluster sizes (the number of members in each cluster). 
+ The code then generates statistics about the clusters and plots 
+ a histogram of the data. It also replaces some of the cluster representatives 
+ produced by MMSeq with new ones from PDB or AFDB, and outputs the top 
+ clusters into a tsv file.
+
+Procedure:
+
+1- Import the pandas library.
+2- Read the vp_clusters_cluster.tsv file into a dataframe.
+3- Read the cluster_sizes.csv file into a dataframe df.
+4- Generate statistics about the clusters using the describe() method and 
+   plot a histogram of the data using the plot() method.
+5- Sort the dataframe by the number of members in each cluster.
+6- Get the top 10,000 cluster representatives from the dataframe.
+7- Calculate the percentage of proteins covered in the top clusters.
+8- Create a new dataframe newClusterReps for the new cluster representatives.
+9- Iterate through the top cluster representatives and check if they have a
+   refseq representative.
+10- If a refseq representative is found, get all the members of the cluster and 
+   check if they have any PDB or AFDB members.
+11- Once a PDB or AFDB member is found, make it the new cluster representative 
+   and add a row to the newClusterReps dataframe.
+12- Augment the topClustersReps dataframe with a new column showing the final 
+   cluster representatives that will be used for folding.
+13- Iterate through the newClusterReps dataframe and assign the new cluster 
+   representative to the corresponding top cluster.
+14- Write the topClustersReps dataframe into a tsv file.
+
+Inputs:
+
+1- vp_clusters_cluster.tsv: A tab-separated file containing information about 
+   protein clusters produced by MMSeq, including a cluster representative and cluster members.
+cluster_sizes.csv: A comma-separated file containing information about cluster 
+   sizes (#of members).
+          
+Outputs:
+
+1- A histogram of the clusters statistics.
+2- top_viral_protein_cluster_reps.tsv: A tab-separated file containing the 
+   top 10,000 cluster representatives and their associated size.
+
+Description Author: ChatGPT, reviewed and corrected by Tamim AlMurad.
+Code author: Tamim AlMurad
+
+Version 1.0
+last review Date: July 11th 2023
+"""
+
+import pandas as pd
+
+#Read clusters file into a dataframe. File is produced by MMSeq with cluster rep and cluster members.
+clustersDF = pd.read_table('vp_clusters_cluster.tsv')
+clustersDF.columns=['Clus_Rep','Clus_Mem']
+
+#Read clusters stats file with cluster rep and the number of members in the cluster.
+df = pd.read_table('cluster_sizes.csv')
+df.columns=['ACCN', '#of members']
+
+#Get statistics of the clusters and plot a histogram of the data.
+df.plot(kind='hist', logy=True, bins=1000)
+stat=df.describe()
+
+#Sort the clusters by the size.
+df.sort_values(by=['#of members'],inplace=True,ascending=False)
+
+
+
+#Get the top 10,000 clusters reps.
+topClustersReps = df.iloc[0:10000]
+
+#%%
+#Percentage of proteins covered in the top clusters. 
+print('10k clusters coverage of the whole viral proteins is %f' 
+      %(topClustersReps['#of members'].sum()/len(clustersDF)*100))
+#%%
+#This section is to replace cluster reps produced by MMSeq to be reps from PDB ot AFDB.
+#Dataframe for the new cluster reps.
+newClusterReps = pd.DataFrame(columns=['old_clus_rep', 'new_clus_rep'])
+
+
+
+for rep in topClustersReps['ACCN']:
+    #Check if the cluster has a refseq rep.
+    if rep.find('.') > 0:
+        
+        #Get all the members of the cluster.
+        tempDF = clustersDF[clustersDF['Clus_Rep'] == rep]
+      
+        for mem in tempDF['Clus_Mem']:
+            #Check if the cluster has any AFDB or PDB members.
+            if mem.find('.') < 0:
+                #Once a PDF or AFDB member is found, make it the cluster rep and break.
+                row = {'old_clus_rep': rep,'new_clus_rep': mem}
+                rowDF=pd.DataFrame.from_dict(row,orient='index').T
+                print("cluster rep is changed \n " + rowDF.to_string())
+                #Add a row showing old cluster rep and new cluster rep.
+                newClusterReps = pd.concat([newClusterReps, rowDF],ignore_index=True)       
+                break
+                        
+
+
+#%%
+#In this section, the df with top cluster reps is augmented with a new column showing the 
+# the final cluster reps that will be used.
+topClustersReps['final_clus_rep']=topClustersReps['ACCN']
+topClustersReps.reset_index()
+
+i =0
+for rep in newClusterReps['old_clus_rep']:
+    #Get the index of the top clusters with new cluster rep.
+    index = topClustersReps[topClustersReps['ACCN']==rep].index.values
+    #Assign the new cluster rep to a top cluster.
+    topClustersReps.loc[index,'final_clus_rep'] = newClusterReps.at[i,'new_clus_rep']
+    i=i+1
+
+print('###Number of clusters rep to be folded is: '+str(len(topClustersReps[topClustersReps['final_clus_rep'].str.contains('\.')])))
+#Write the top clusters dataframe into a tsv file.
+topClustersReps.to_csv('top_viral_protein_cluster_reps_ver.tsv',sep='\t')
